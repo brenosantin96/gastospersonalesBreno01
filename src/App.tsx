ddfe04c741a13d2c2efba1f8e3576f4d56b64236
Breno Santin <brenolimaa96@gmail.com>
2022-09-29 16:12:43 -0300

    adding things

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import * as C from './app.styles'
+import { CategorieType } from './types/Categorie'
+import { ItemType } from './types/Item'
+import { categories } from './data/categories'
+import { items } from './data/items'
+import { getCurrentMonth } from './utilities/dateFilter'
 
 function App() {
+
+  const [list, setList] = useState<ItemType[]>(items);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        
-      {/* área de informacoes */}
-     
-      {/* área de inserção */}
-     
-      {/* área de lista */}
+
+        {/* área de informacoes */}
+
+        {/* área de inserção */}
+
+        {/* área de lista */}
 
       </C.Body>
     </C.Container>
